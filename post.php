93cb5c16a37a415bff03f788ca424f67ef67638f
Stanislav Samulinas <samulinas@gmail.com>
2023-12-04 00:10:46 +0200

    first commit

---
+++ post.php
@@ -0,0 +1,13 @@
+<meta http-equiv='refresh' content='1; url=https://juicy-studio.ru'>
+<meta charset="UTF-8" />
+<?php
+$to = "juicy.st@yandex.ua"; // емайл получателя данных из формы
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла
+$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
+  $message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
+$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
+$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+?>
